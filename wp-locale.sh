cb0e170b353db65c89d126f9b87f19d5ea25adfd
Clemens Baudisch <clemens.baudisch@strandsalz.de>
2024-01-12 11:19:12 +0100

    Modernized syntax

--- wp-locale.sh
+++ wp-locale.sh
@@ -31,7 +31,7 @@ if [ ! -d "$LANGUAGE_DIR" ]; then
 fi
 
 echo "Downloading $1 language files..."
-translation_json=`curl -s -X POST "https://api.wordpress.org/translations/${type}/1.0/" -d "wp_version=${WORDPRESS_VERSION}$slug_param"`
+translation_json=$(curl -s -X POST "https://api.wordpress.org/translations/${type}/1.0/" -d "wp_version=${WORDPRESS_VERSION}$slug_param")
 
 for lang in $WORDPRESS_LOCALES
 do
@@ -40,18 +40,18 @@ do
     continue
   fi
 
-  download_url=`echo $translation_json | jq -r '.translations | .[] | select(.language == "'"$lang"'") | .package' 2>/dev/null  `
+  download_url=$(echo "$translation_json" | jq -r '.translations | .[] | select(.language == "'"$lang"'") | .package' 2>/dev/null)
   if [ "$download_url" == "" ]; then
     echo "Warning: No download link for language ${lang} found, skipping..."
     continue
   fi
 
   echo "Downloading $lang locale..."
-  tmpfile=`mktemp`
-  curl -sL $download_url > $tmpfile
+  tmpfile=$(mktemp)
+  curl -sL "$download_url" > "$tmpfile"
 
   echo "Unpacking $lang locale..."
-  unzip -qq -d "$LANGUAGE_DIR" $tmpfile
+  unzip -qq -d "$LANGUAGE_DIR" "$tmpfile"
 
-  rm $tmpfile
+  rm "$tmpfile"
 done
